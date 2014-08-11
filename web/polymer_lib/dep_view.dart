a1e63dc982db675a47553e1204fb9b5a020a8206
Ty Overby <tyoverby@google.com>
2014-08-11 14:09:17 -0700

    Adds a rough dependency viewer and a tab-based UI.
    This change also reworks the UI to be based on paper-elements.

--- web/polymer_lib/dep_view.dart
+++ web/polymer_lib/dep_view.dart
@@ -9,18 +9,18 @@ import '../infohelper.dart';
 class DepView extends PolymerElement {
   InfoHelper _dumpInfo;
   String _currentlyTargeting;
-  
-  TableElement ownersTable;
-  TableElement currentTable;
-  TableElement ownedTable;
-  
+
+  TableSectionElement ownersTable;
+  TableSectionElement currentTable;
+  TableSectionElement ownedTable;
+
   DepView.created() : super.created() {}
-  
+
   set target(String id) {
     _currentlyTargeting = id;
     _populate(id);
   }
-  
+
   String get target {
     return _currentlyTargeting;
   }
@@ -30,38 +30,52 @@ class DepView extends PolymerElement {
     dv.dumpInfo = dumpinfo;
     return dv;
   }
-  
+
   set dumpInfo(InfoHelper dumpInfo) {
+    TableSectionElement getTbody(TableElement table) =>
+      table.querySelector("tbody");
+
     _dumpInfo = dumpInfo;
-    ownersTable = $['owners'];
-    currentTable = $['current'];
-    ownedTable = $['owned'];
+    ownersTable = getTbody($['owners']);
+    currentTable = getTbody($['current']);
+    ownedTable = getTbody($['owned']);
   }
-  
+
   TableRowElement _generateRow(String id) {
     List<String> path = _dumpInfo.path(id);
-    return new TableRowElement()..children.addAll(
-        [
-           new TableCellElement()..text = path.join("."),
-           new TableCellElement()..text = 
-            _dumpInfo.reverseDependencies(id).length.toString()
-        ]);
+    // TODO(TyOverby): Make a polymer element to abstract this mess
+    return new TableRowElement()
+      ..children.addAll([
+          // Name Column
+          new TableCellElement()
+            ..text = path.join("."),
+          // Stats Column
+          new TableCellElement()
+            ..children.add(
+              new SpanElement()
+                 ..text = "↖ ${_dumpInfo.reverseDependencies(id).length} | " +
+                           "${_dumpInfo.dependencies(id).length} ↘"
+                 ..style.float = "right"
+          )
+        ])
+        ..onClick.listen((_)=> this.target = id);
   }
-  
+
   void _populate(String id) {
     ownersTable.children.clear();
     currentTable.children.clear();
     ownedTable.children.clear();
-    
+
     List<String> owners = _dumpInfo.reverseDependencies(id);
     List<String> owned = _dumpInfo.dependencies(id);
-    
+
     Iterable<TableRowElement> sortedRows(Iterable<String> ids) {
-      var sorted = ids.toList()..sort((id1, id2) => 
-          _dumpInfo.reverseDependencies(id1).length - 
+      var sorted = ids.toList()..sort((id1, id2) =>
+          _dumpInfo.reverseDependencies(id1).length -
           _dumpInfo.reverseDependencies(id2).length);
       return sorted.map(_generateRow);
     }
+
     ownersTable.children.addAll(sortedRows(owners));
     currentTable.children.add(_generateRow(id));
     ownedTable.children.addAll(sortedRows(owned));
--- web/viewer.dart
+++ web/viewer.dart
@@ -3,8 +3,10 @@ library viewer;
 import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
+import 'dart:js';
 import 'package:polymer/polymer.dart';
 import 'package:paper_elements/paper_tab.dart';
+import 'package:paper_elements/paper_ripple.dart';
 
 import './format_versions/versions.dart';
 import './polymer_lib/tree_table.dart';
@@ -13,31 +15,47 @@ import './infohelper.dart';
 
 part './dragdrop.dart';
 
-List<String> slides = const ["info", "hier", "dep", "load"];
+final List<String> slides = const ["info", "hier", "dep", "load"];
 
 void _noSlide() {
+  // Disable all of the slides and tabs
   for (String id in slides) {
     var slide = document.querySelector("#$id-slide");
     slide.style.opacity = "0";
-    slide.style.height = '0';
+    slide.style.left = "100px";
+    slide.style.maxHeight = "0px";
+
     var tab = document.querySelector("#$id-tab");
-    if(tab != null) {
+    if (tab != null) {
       tab.classes.remove("core-selected");
     }
   }
 }
 
-void _switchSlide(String id) {
+void _switchSlide(String id, {bool fromMouse: false}) {
   _noSlide();
   var slide = document.querySelector("#$id-slide");
-  slide.style.opacity = "1";
-  slide.style.height = "auto";
-  var tab = document.querySelector("#$id-tab");
-  if (tab != null) {
-    tab.classes.add("core-selected");
-    var tabs = document.querySelector("paper-tabs");
-    tabs.attributes['selected'] = tab.attributes['offset'];
-  }
+  slide.style.maxHeight = "10000px";
+  new Timer(const Duration(milliseconds: 150), () {
+    slide.style.opacity = "1";
+    slide.style.left = "0px";
+    var tab = document.querySelector("#$id-tab");
+    if (tab != null) {
+      tab.classes.add("core-selected");
+      var tabs = document.querySelector("paper-tabs");
+      tabs.attributes['selected'] = tab.attributes['offset'];
+
+      // Draw a ripple on the tab if we didn't already click on it.
+      if (!fromMouse) {
+        PaperRipple ripple =
+            tab.shadowRoot.querySelector("paper-ripple") as PaperRipple;
+        var pos = {'x': tab.offsetLeft + tab.clientWidth / 2, 'y': 0};
+        ripple.jsElement.callMethod("downAction", [new JsObject.jsify(pos)]);
+        window.animationFrame.then((_) =>
+            ripple.jsElement.callMethod("upAction", []));
+      }
+    }
+  });
 }
 
 main() {
@@ -54,11 +72,10 @@ main() {
 
     List<PaperTab> tabs = querySelectorAll("paper-tab") as List<PaperTab>;
     for (PaperTab tab in tabs) {
-      print(tab);
       tab.onClick.listen((_){
         String link = tab.attributes["slide"];
         if (link != null) {
-          _switchSlide(link);
+          _switchSlide(link, fromMouse: true);
         }
       });
     }
