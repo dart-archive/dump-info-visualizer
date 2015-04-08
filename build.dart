0ab65d7b7f278b2953ff0ec441a9ca046ea6d136
Kevin Moore <kevmoo@google.com>
2015-04-08 11:40:17 -0700

    rename entrypoint to web/index.html

--- build.dart
+++ build.dart
@@ -1,6 +1,6 @@
 import 'package:polymer/builder.dart';
-        
+
 main(args) {
-  build(entryPoints: ['web/viewer.html'],
+  build(entryPoints: ['web/index.html'],
         options: parseOptions(args));
 }
